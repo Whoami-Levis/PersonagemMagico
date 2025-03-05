fe7fe1debce90856fb26756c0808857078954ad0
Levis <rm559328@fiap.com.br>
2025-03-05 20:22:33 -0300

    Construtor

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        //Criando um objeto do tipo Personagem Mágico
-        PersonagemMagico mago = new PersonagemMagico();
+        //Criando um objeto do tipo Personagem Mágico (Mago)
+        PersonagemMagico mago = new PersonagemMagico("Gandalf", 100, "Magia");
 
-        //Atribuindo Valores aos atributos do objeto
-        mago.nome = "Gandalf";
-        mago.nivelEnergia = 100;
-        mago.Poder = "Magia";
+        //Exibindo valores dos atributos do objeto (Mago)
+        System.out.println(mago.nome + " - Energia:" + mago.nivelEnergia + " - Poder:" + mago.Poder);
 
-        //Exibindo valores dos atributos do objeto
-        System.out.println(mago.nome + " Energia:" + mago.nivelEnergia + " Poder:" + mago.Poder);
+        //Criado novo personagem magico (Elfo)
+        PersonagemMagico elfo = new PersonagemMagico("Elfo", 100, "Arqueiro");
 
-        //Criado novo personagem magico (elfo)
-        PersonagemMagico elfo = new PersonagemMagico();
-        //Definidno os valores do atributos do objeto, pegando o molde da classe Persongem Magico
-        elfo.nome = "Legolas";
-        elfo.nivelEnergia = 50;
-        elfo.Poder = "Arqueiro";
-        //Imprimindo os valores do persdnagem elfo
-        System.out.println(elfo.nome + "Energia:" + elfo.nivelEnergia + " Poder:" + elfo.Poder);
+
+        //Imprimindo os valores do persdnagem (Elfo)
+        System.out.println(elfo.nome + " - Energia:" + elfo.nivelEnergia + " - Poder:" + elfo.Poder);
     }
 }
--- src/PersonagemMagico.java
+++ src/PersonagemMagico.java
@@ -1,5 +1,19 @@
 public class PersonagemMagico {
+
+    //Atrubutos da Classe Personagem Magico
     String nome;
+
     int nivelEnergia;
+
     String Poder;
+
+    //Criação do Construtor
+    public PersonagemMagico ( String nome , int nivelEnergia , String Poder ) {
+        this.nome = nome;
+        this.nivelEnergia = nivelEnergia;
+        this.Poder = Poder;
+
+    }
 }
+
+
